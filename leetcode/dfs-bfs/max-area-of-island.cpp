d6f1c5e8be5b0549013912212bdd328c92318723
SC0d3r <sasan.kelishani@gmail.com>
2022-08-05 03:38:03 +0430

    solved the dfs-bfs -> max area of island

---
+++ leetcode/dfs-bfs/max-area-of-island.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    inline int maxAreaOfIsland(std::vector<std::vector<int>> &grid)
+    {
+        const auto m = grid.size();
+        const auto n = grid[0].size();
+        int max = 0;
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                int accum = 0;
+                max = std::max(max, getArea(grid, m, n, i, j, accum));
+            }
+        }
+        return max;
+    }
+
+private:
+    inline int getArea(std::vector<std::vector<int>> &grid, int m, int n, int i, int j, int &accum)
+    {
+
+        if (i < 0 || i >= m || j < 0 || j >= n)
+            return 0;
+
+        // 2 is the visited number for the cells in the grid
+        if (grid[i][j] == 0 || grid[i][j] == 2)
+            return 0;
+
+        grid[i][j] = 2; // visited
+        accum++;
+
+        getArea(grid, m, n, i + 1, j, accum);
+        getArea(grid, m, n, i - 1, j, accum);
+        getArea(grid, m, n, i, j + 1, accum);
+        getArea(grid, m, n, i, j - 1, accum);
+
+        return accum;
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::vector<std::vector<int>> grid =
+        {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+         {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+         {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+
+    std::cout << "area: " << s.maxAreaOfIsland(grid) << std::endl;
+    return 0;
+}
