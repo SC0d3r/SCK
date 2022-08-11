6ec2ef53c94be71f692499afd6bb96fe528f12b7
SC0d3r <sasan.kelishani@gmail.com>
2022-08-12 00:09:11 +0430

    add another method to solve number of 1 bits in bit manipulation

--- leetcode/bit-manipulation/Number-of-1-Bits.cpp
+++ leetcode/bit-manipulation/Number-of-1-Bits.cpp
@@ -14,24 +14,35 @@ public:
 		// method 2: using bit shifting
 		// runtime: 0ms
 
-		int res = 0;
-		for (int i = 0; i < 32; i++)
-		{
-			if (n == 0)
-				return res;
-			const auto x = n >> 1;
-			if (x << 1 != n)
-				res++;
-			n = x;
-		}
+		// int res = 0;
+		// for (int i = 0; i < 32; i++)
+		// {
+		// 	if (n == 0)
+		// 		return res;
+		// 	const auto x = n >> 1;
+		// 	if (x << 1 != n)
+		// 		res++;
+		// 	n = x;
+		// }
 
-		return res;
+		// return res;
+
+		// method 3: crazy stuff
+		// runtime: ??ms
+
+		n = n - ((n >> 1) & 0x55555555);
+		n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+		n = (n + (n >> 4)) & 0x0F0F0F0F;
+		n = n + (n >> 8);
+		n = n + (n >> 16);
+		return n & 0x0000003F;
 	}
 };
 
 int main()
 {
 	Solution s;
-	std::cout << "count: " << s.hammingWeight(7) << std::endl;
+	uint32_t n = 7;
+	std::cout << "count: " << s.hammingWeight(n) << std::endl;
 	return 0;
 }
